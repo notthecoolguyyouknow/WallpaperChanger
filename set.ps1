98c7ac8e9c2ab72fea82d3eb3a7c3fed2cf7cfd3
iakzs <k@iakzs.is-a.dev>
2024-09-29 11:01:23 -0300

    updated comments

--- set.ps1
+++ set.ps1
@@ -11,7 +11,7 @@ $delayHours = 3
 function Get-ScriptPath {
     if (Test-Path $scriptPath1) { return $scriptPath1 }
     #elseif (Test-Path $scriptPath2) { return $scriptPath2 }
-    #elseif (Test-Path $scriptPath3) { return $scriptPath3 }
+    elseif (Test-Path $scriptPath3) { return $scriptPath3 }
     else { return $null }
 }
 

